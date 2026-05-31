a21d53d2013eba4ad04fce3b913a80ded6847f39
alexyaisuui <alexyai.su@gmail.com>
2026-05-31 19:32:16 +0100

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,22 +1,51 @@
 import fetch from 'node-fetch'
+
 let handler = async (m, { conn }) => {
-  let caption = `
-*「 معلومات عن صاحب البوت 」*\n\n
-*Whatsapp channel:*\n https://whatsapp.com/channel/0029VaX4b6J7DAWqt3Hhu01A\n
-*instagram:*\ninstagram.com/noureddine_ouafy
+    const ownerNumber = '212709138520'
+    const channelUrl = 'https://whatsapp.com/channel/0029Vb7nYRZHAdNWqXl8ug1b'
+    const thumbUrl = 'https://files.catbox.moe/i8ntv0.jpg' // غيرها لصورتك لو حاب
 
-*youtube:*\nyoutube.com/@noureddineouafy2
+    let caption = `
+*🗂️ معلومات مطور البوت*
 
-*facebook page:*\nwww.facebook.com/profile.php?id=100063533185520
+👨‍💻 *المطور:* 
+wa.me/${ownerNumber}
 
-*script bot :* github.com/noureddineouafy
+✨ *قناة الواتساب:* 
+${channelUrl}
 
+📞 اضغط الزر تحت للتواصل مباشرة
 `.trim()
-  m.reply(caption)
+
+    // إرسال بصورة + أزرار
+    await conn.sendMessage(m.chat, {
+        image: { url: thumbUrl },
+        caption: caption,
+        buttons: [
+            { buttonId: `wa.me/${ownerNumber}`, buttonText: { displayText: '' }, type: 1 },
+            { buttonId: channelUrl, buttonText: { displayText: '' }, type: 1 }
+        ],
+        footer: 'Owner Command',
+        headerType: 4
+    }, { quoted: m })
+
+    // إرسال جهة اتصال المطور كـ vCard زيادة
+    const vcard = `BEGIN:VCARD
+VERSION:3.0
+FN:Bot Owner
+TEL;waid=${ownerNumber}:${ownerNumber}
+END:VCARD`
+    await conn.sendMessage(m.chat, { 
+        contacts: { 
+            displayName: 'Bot Owner', 
+            contacts: [{ vcard }] 
+        }
+    }, { quoted: m })
 }
+
 handler.help = ['owner']
 handler.tags = ['infobot']
-handler.command = /^(owner)$/i
+handler.command = /^(owner|مطور)$/i
 handler.limit = false
 
 export default handler
