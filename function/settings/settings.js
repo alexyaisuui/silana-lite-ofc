474601c589bca72c1b0b0a3f42c7bd3aa590ebfe
alexyaisuui <alexyai.su@gmail.com>
2026-06-15 19:32:12 +0100

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,10 +14,10 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212704144087',
- pairingNumber : '212704144087',
+ nomerbot : '212700768989',
+ pairingNumber : '212700768989',
  figlet: 'Alexy-Ai', // create a start console display
- nomorwa : '212704144087',
+ nomorwa : '212700768989',
  nameown : 'MANON TECH',
  nomerown : '212709138520',
  packname : 'sticker by ',
